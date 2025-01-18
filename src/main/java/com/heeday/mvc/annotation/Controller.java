82d5fc8b5354eecf115c39c237a20af06c093f9c
heedayday <heeday3001@gmail.com>
2025-01-18 21:04:10 +0900

    어노테이션 javadoc

--- src/main/java/com/heeday/mvc/annotation/Controller.java
+++ src/main/java/com/heeday/mvc/annotation/Controller.java
@@ -5,9 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/*커스텀 어노테이션 https://velog.io/@potato_song/Java-%EC%96%B4%EB%85%B8%ED%85%8C%EC%9D%B4%EC%85%98-%EC%BB%A4%EC%8A%A4%ED%85%80-%EC%96%B4%EB%85%B8%ED%85%8C%EC%9D%B4%EC%85%98-%EB%A7%8C%EB%93%A4%EA%B8%B0
-@Retention 어노테이션 메타정보가 언제 버려질지에 대한 타이밍을 설정함 https://jake-seo-dev.tistory.com/67
-@Target https://jangjjolkit.tistory.com/39 https://curiousjinan.tistory.com/entry/spring-custom-annotation*/
+/** <a href="https://curiousjinan.tistory.com/entry/spring-custom-annotation">커스텀 어노테이션</a>
+* <a href="https://jake-seo-dev.tistory.com/67">@Retention</a> 어노테이션 메타정보가 언제 버려질지에 대한 타이밍을 설정함
+* <a href="https://jangjjolkit.tistory.com/39">@Target</a>
+ */
 
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
